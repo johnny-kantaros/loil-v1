4580d8e3aeaf1dda314e21a11894db60a5f92798
Johnny Kantaros <jkantaros@middlebury.edu>
2023-11-06 15:05:52 -0500

    example logo for demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className='flex justify-center p-10'>
       <Image src='/mark.jpeg' alt='Profile Picture' width={200} height={400}/>
+      <h1 className='text-black text-xl p-10'>Content goes here</h1>
     </div>
     
   )
