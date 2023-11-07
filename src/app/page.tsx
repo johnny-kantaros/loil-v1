053c4913a43937d6b02b67dfa20c426cf41b6bbf
Johnny Kantaros <jkantaros@middlebury.edu>
2023-11-06 20:01:05 -0500

    removed prof pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='flex justify-center p-10'>
-      <Image src='/mark.jpeg' alt='Profile Picture' width={200} height={400}/>
+      
       <h1 className='text-black text-xl p-10'>Content goes here</h1>
     </div>
     
